b69435920f58cc62154cff31fe58de6e9c8802ff
Rajesh R J <rajeshrj1981@yahoo.co.in>
2016-02-28 15:30:13 +0530

    firbase url changed

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,5 @@
 // Ionic Starter App
-var fb = new Firebase("https://banquetapp.firebaseio.com/");
+var fb = new Firebase("https://co-pay.firebaseio.com/");
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
